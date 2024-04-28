245dd2b0f7752c5b027e0f3d29d6c4900716cc46
Amir Ali anwar <amiralianwar611@gmail.com>
2024-04-28 22:55:38 +0500

    task app

---
+++ src/task.ts
@@ -0,0 +1,4 @@
+const btn= document.querySelector<HTMLFormElement>('.form')
+const formInput= document.querySelector('.form-input')! as HTMLInputElement
+
+const taskListElement= document.querySelector<HTMLUListElement>('.list')
