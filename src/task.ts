d2a4647d356272065adc478b68ac703cb73eee7b
Amir Ali anwar <amiralianwar611@gmail.com>
2024-04-29 22:44:12 +0500

    working on the task App

--- src/task.ts
+++ src/task.ts
@@ -1,4 +1,44 @@
-const btn= document.querySelector<HTMLFormElement>('.form')
-const formInput= document.querySelector('.form-input')! as HTMLInputElement
+const taskForm = document.querySelector<HTMLFormElement>('.form')
+const formInput = document.querySelector('.form-input')! as HTMLInputElement
 
-const taskListElement= document.querySelector<HTMLUListElement>('.list')
+const taskListElement = document.querySelector<HTMLUListElement>('.list')
+
+type Task = {
+    description: string,
+    isCompleted: boolean
+}
+
+const tasks: Task[] = []
+function createTask(event: SubmitEvent) {
+    event.preventDefault()
+    const taskDescription = formInput.value
+    if (taskDescription) {
+
+        const task: Task = {
+            description: taskDescription,
+            isCompleted: false
+        }
+        addTask(task)
+        renderTask(task)
+        updateStorage()
+        formInput.value = '';
+        return ''
+
+    }
+    alert('Please enter a task description')
+}
+function addTask(task: Task): void {
+    console.log(task);
+    
+    tasks.push(task)
+}
+function renderTask(task:Task):void{
+   const taskElement= document.createElement('li')
+   taskElement.textContent=task.description
+   taskListElement?.appendChild(taskElement)
+
+}
+function updateStorage():void{
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+}
+taskForm?.addEventListener('submit', createTask)
