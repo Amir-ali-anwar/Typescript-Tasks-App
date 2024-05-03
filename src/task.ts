8b7ccd684b8a58293e43d0e0e8dd1d1e77548b31
Amir Ali Anwar <amir.dev@pk.see.biz>
2024-05-02 22:32:29 -0700

    working on the task app

--- src/task.ts
+++ src/task.ts
@@ -1,57 +1,63 @@
-const taskForm = document.querySelector<HTMLFormElement>('.form')
-const formInput = document.querySelector('.form-input')! as HTMLInputElement
-
-const taskListElement = document.querySelector<HTMLUListElement>('.list')
-
+const taskForm = document.querySelector<HTMLFormElement>(".form");
+const formInput = document.querySelector(".form-input")! as HTMLInputElement;
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+const iconToggle = document.querySelector<HTMLElement>(
+  ".theme-icons .fa-solid"
+);
+const root = document.querySelector<HTMLElement>(":root");
 type Task = {
-    description: string,
-    isCompleted: boolean
-}
+  description: string;
+  isCompleted: boolean;
+};
 
 function loadTasks(): Task[] {
-    const storedItems = localStorage.getItem('tasks')
-    return storedItems ? JSON.parse(storedItems) : []
+  const storedItems = localStorage.getItem("tasks");
+  return storedItems ? JSON.parse(storedItems) : [];
 }
 const tasks: Task[] = loadTasks();
-tasks.forEach(renderTask)
+tasks.forEach(renderTask);
 function createTask(event: SubmitEvent) {
-    event.preventDefault()
-    const taskDescription = formInput.value
-    if (taskDescription) {
-
-        const task: Task = {
-            description: taskDescription,
-            isCompleted: false
-        }
-        addTask(task)
-        renderTask(task)
-        updateStorage()
-        formInput.value = '';
-        return ''
-
-    }
-    alert('Please enter a task description')
+  event.preventDefault();
+  const taskDescription = formInput.value;
+  if (taskDescription) {
+    const task: Task = {
+      description: taskDescription,
+      isCompleted: false,
+    };
+    addTask(task);
+    renderTask(task);
+    updateStorage();
+    formInput.value = "";
+    return "";
+  }
+  alert("Please enter a task description");
 }
+iconToggle?.addEventListener("click", toggleTheme);
+
+function toggleTheme(event: Event): void {
+  event.stopPropagation();
+  if (root!==null) {
+    const rootStyles = getComputedStyle(root);
+  }
+}
+
 function addTask(task: Task): void {
-    console.log(task);
-    
-    tasks.push(task)
-}
-function renderTask(task:Task):void{
-   const taskElement= document.createElement('li')
-   const iconElement= document.createElement('i')
-   console.log({iconElement});
-   
-    iconElement.setAttribute('class','fa-solid fa-user')
-//    input.type='checkbox'
-//    input.checked= task.isCompleted
-   taskElement.appendChild(iconElement)
-   taskElement.appendChild(iconElement)
-   taskElement.textContent=task.description
-   taskListElement?.appendChild(taskElement)
+  console.log(task);
 
+  tasks.push(task);
 }
-function updateStorage():void{
-    localStorage.setItem('tasks',JSON.stringify(tasks))
+function renderTask(task: Task): void {
+  const taskElement = document.createElement("li");
+  const taskElementParagparh = document.createElement("p");
+  const iconElement = document.createElement("i");
+  taskElement.appendChild(taskElementParagparh);
+  iconElement.setAttribute("class", "fa-solid fa-trash");
+  taskElement.appendChild(iconElement);
+  taskElementParagparh.textContent = task.description;
+  taskListElement?.appendChild(taskElement);
+  taskListElement?.appendChild(taskElement);
 }
-taskForm?.addEventListener('submit', createTask)
+function updateStorage(): void {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+taskForm?.addEventListener("submit", createTask);
