2d6c41280a717ae4a370b1ac14569fa01a459de0
Amir Ali Anwar <amiralianwar611@gmail.com>
2024-05-04 08:45:26 -0700

    Toggle theme successfull

--- src/task.ts
+++ src/task.ts
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 const taskForm = document.querySelector<HTMLFormElement>(".form");
 const formInput = document.querySelector(".form-input")! as HTMLInputElement;
 const taskListElement = document.querySelector<HTMLUListElement>(".list");
@@ -15,9 +17,9 @@ type ThemeTyes={
   newWhiteColor:string
 
 }
-document.addEventListener("DOMContentLoaded", function () {
-  const theme = getThemeFromLocalStorage();
-});
+let newBackgroundColor = '';
+let newTextColor = '';
+let newWhiteColor = '';
 function loadTasks(): Task[] {
   const storedItems = localStorage.getItem("tasks");
   return storedItems ? JSON.parse(storedItems) : [];
@@ -38,39 +40,63 @@ function createTask(event: SubmitEvent) {
     formInput.value = "";
     return "";
   }
-  alert("Please Enter task description");
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Please Enter the Task description!",
+  });
 }
 iconToggle?.addEventListener("click", toggleTheme);
 
+
+
+function themeFromLocalStorage() {
+  const storedTheme = getThemeFromLocalStorage();
+  if (root !== null) {
+    if (storedTheme) {
+      root.style.setProperty(
+        "--background-color",
+        storedTheme?.newBackgroundColor
+      );
+      root.style.setProperty("--text-color", storedTheme?.newTextColor);
+      root.style.setProperty("--white", storedTheme?.newWhiteColor);
+      return;
+    }
+  }
+}
+document.addEventListener('DOMContentLoaded',themeFromLocalStorage)
+
 function toggleTheme(event: Event): void {
   event.stopPropagation();
+
   if (root !== null) {
     const rootStyles = getComputedStyle(root);
     const currentBackgroundColor = rootStyles.getPropertyValue('--background-color').trim();
-    
-    let newBackgroundColor = '';
-    let newTextColor = '';
-    let newWhiteColor = '';
+   
 
     // Toggle between light and dark themes
     if (currentBackgroundColor === '#f8fafc') {
       newBackgroundColor = '#282c35'; // Dark background
       newTextColor = '#fff'; // Light text
       newWhiteColor = '#282c35'; // White color
+      root.style.setProperty('--background-color', newBackgroundColor);
+      root.style.setProperty('--text-color', newTextColor);
+      root.style.setProperty('--white', newWhiteColor);
       iconToggle?.classList.remove('fa-moon'); // Remove moon icon
       iconToggle?.classList.add('fa-sun'); // Add sun icon
     } else {
       newBackgroundColor = '#f8fafc'; // Light background
       newTextColor = '#0f172a'; // Dark text
       newWhiteColor = '#fff'; // White color
+      root.style.setProperty('--background-color', newBackgroundColor);
+      root.style.setProperty('--text-color', newTextColor);
+      root.style.setProperty('--white', newWhiteColor);
       iconToggle?.classList.remove('fa-sun'); // Remove sun icon
       iconToggle?.classList.add('fa-moon'); // Add moon icon
     }
 
     // Update CSS variables
-    root.style.setProperty('--background-color', newBackgroundColor);
-    root.style.setProperty('--text-color', newTextColor);
-    root.style.setProperty('--white', newWhiteColor);
+   
     const themevalues={
       newBackgroundColor,
       newTextColor,
@@ -102,7 +128,7 @@ function updateStorage(): void {
 function UpdateThemeLocalStorage(theme:ThemeTyes):void{
   localStorage.setItem('theme', JSON.stringify(theme))
 }
-function getThemeFromLocalStorage():ThemeTyes[]{
+function getThemeFromLocalStorage(): ThemeTyes | ThemeTyes {
   const storedItems = localStorage.getItem("theme");
   return storedItems ? JSON.parse(storedItems) : [];
 }
